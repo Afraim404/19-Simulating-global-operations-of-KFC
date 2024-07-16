d91441584c5860f2eac567c72b356fea237672b4
Afraim404 <afraimuzzaman19@gmail.com>
2024-07-16 09:02:06 -0700

    fxml update

---
+++ src/main/java/com/example/demo/ResturantManagerViewController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+
+public class ResturantManagerViewController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void homeOnaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewinventoryonaction(ActionEvent actionEvent) {
+    }
+}
