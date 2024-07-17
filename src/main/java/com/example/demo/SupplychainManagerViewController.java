fc5c94b33138e2433e7afddbdb4f9f1c073dfde6
Afraim404 <afraimuzzaman19@gmail.com>
2024-07-17 02:50:12 -0700

    supplychain_Manager_view .fxml update

---
+++ src/main/java/com/example/demo/SupplychainManagerViewController.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+
+public class SupplychainManagerViewController
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void qualitycontrollreportonaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void creatnewpurchaseonaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewrequestedproductlsitonaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateinventoryonaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void homeOnaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void supplierlistonaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void cordinatetransportonaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void uploadlpricelistonaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void setpriorityproductlistonaction(ActionEvent actionEvent) {
+    }
+}
