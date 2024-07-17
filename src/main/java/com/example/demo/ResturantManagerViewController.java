854df453441b456f575edc0d57c9bfac58556160
Afraim404 <afraimuzzaman19@gmail.com>
2024-07-17 02:12:48 -0700

    resturant_Manager_View.fxmlfxml update

--- src/main/java/com/example/demo/ResturantManagerViewController.java
+++ src/main/java/com/example/demo/ResturantManagerViewController.java
@@ -15,4 +15,32 @@ public class ResturantManagerViewController
     @javafx.fxml.FXML
     public void viewinventoryonaction(ActionEvent actionEvent) {
     }
+
+    @javafx.fxml.FXML
+    public void grantleaveonaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void salesreportyonaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void costomerfeedbackonaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void employeeschedulesonaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void employeeperformanceonaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void managingmenueonaction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void maeketingstrategyonaction(ActionEvent actionEvent) {
+    }
 }
